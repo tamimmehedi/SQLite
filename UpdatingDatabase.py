c6d82b3621474a913543e24b37fa5e10750d12b9
MD Tamim Mehedi <43099464+tamimmehedi@users.noreply.github.com>
2018-11-02 08:57:39 +0100

    Create UpdatingDatabase.py
    I have updated the previous database file with some functions.

---
+++ UpdatingDatabase.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def create_table():
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS store(item TEXT,quantity INTEGER,price REAL)")
+    conn.commit()
+    conn.close()
+def insert(item,quantity,price):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("INSERT INTO store VALUES(?,?,?)",(item,quantity,price))
+    conn.commit()
+    conn.close()
+insert("Coffee Cup",10,5)
